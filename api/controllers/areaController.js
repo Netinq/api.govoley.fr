e5f0f62ccc4fac1d20e914abfa7e169bd95b0fe7
BuildTools <sarquentin@gmail.com>
2022-06-15 03:14:42 +0200

    :rocket: Adding area store function

--- api/controllers/areaController.js
+++ api/controllers/areaController.js
@@ -1,17 +1,28 @@
 const ErrorMessage = require('../constructors/ErrorMessage');
-
+const SuccessMessage = require('../constructors/SuccessMessage');
+const NodeGeocoder = require('node-geocoder');
 const Areas = require('../../database/models/Areas');
 
 async function Store(req, res) {
 
-  const adress = 
+  const options = {
+    provider: 'openstreetmap',
+  };
+
+  const geocoder = NodeGeocoder(options);
+  const json = await geocoder.reverse({lat: req.body.latitude, lon: req.body.longitude})
+  const adress = `${json[0].neighbourhood} ${json[0].streetName}, ${json[0].zipcode} ${json[0].city}, ${json[0].country}`
 
   const area = new Areas({
-    area_surface: req.area_surface,
-    areas_nb: req.areas_nb,
+    area_surface: req.body.area_surface,
+    areas_nb: req.body.areas_nb,
     adress: adress,
-    latitude: req.latitude,
-    longitude: req.longitude,
+    latitude: req.body.latitude,
+    longitude: req.body.longitude,
+  })
+
+  area.save().then(() => {
+    new SuccessMessage('Area registered').send(res)
   })
 }
 

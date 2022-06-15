abbe9420889a3de87e864d48696c8896caa98f62
BuildTools <sarquentin@gmail.com>
2022-06-15 03:14:59 +0200

    :ambulance: Fixing bad call

--- api/middlewares/AppToken.js
+++ api/middlewares/AppToken.js
@@ -1,4 +1,4 @@
-const { ErrorMessage } = require('../constructors/ErrorMessage');
+const ErrorMessage = require('../constructors/ErrorMessage');
 
 module.exports = async (req, res, next) => {
   const token = req.header("app-token");

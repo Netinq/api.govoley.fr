23c28a163865122fb87c22ba5bb17a643be53811
BuildTools <sarquentin@gmail.com>
2022-06-21 01:24:33 +0200

    :recycle: Light refactor

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -6,50 +6,46 @@ const bcrypt = require('bcryptjs');
 const User = require('../../database/models/User');
 const JWT = require('../tools/jwt');
 
-async function Login(request, response)
+async function Login(req, res)
 {
-  const {error} = validatorLogin.validate(request.body);
-  if (error) return new ErrorMessage(error.details[0].message,error.details[0].context.key).send(response);
+  const {error} = validatorLogin.validate(req.body);
+  if (error) return new ErrorMessage(error.details[0].message,error.details[0].context.key).send(res);
 
-  const user = await User.findOne({ where: {email: request.body.email},attributes: {include: ['password']}});
-  if (!user) return new ErrorMessage('Nous ne trouvons pas l\'adresse email !', 'email').send(response);
+  const user = await User.findOne({ where: {email: req.body.email},attributes: {include: ['password']}});
+  if (!user) return new ErrorMessage('Nous ne trouvons pas l\'adresse email !', 'email').send(res);
 
-  const verifyPasswd = await bcrypt.compare(request.body.password, user.password);
-  if (!verifyPasswd) return new ErrorMessage('Le mot de passe est invalide !', 'password').send(response);
+  const verifyPasswd = await bcrypt.compare(req.body.password, user.password);
+  if (!verifyPasswd) return new ErrorMessage('Le mot de passe est invalide !', 'password').send(res);
 
   const token = JWT(user);
 
-  return response.json({
-    token: token,
-  })
+  return res.json({token: token})
 }
 
-async function Register(request, response)
+async function Register(req, res)
 {
-  const {error} = validatorRegister.validate(request.body);
-  if (error) return new ErrorMessage(error.details[0].message,error.details[0].context.key).send(response);
+  const {error} = validatorRegister.validate(req.body);
+  if (error) return new ErrorMessage(error.details[0].message,error.details[0].context.key).send(res);
 
-  const exist = await User.findOne({ where: {email: request.body.email} });
-  if(exist) return new ErrorMessage('Un compte existe déjà avec cette adresse email !', 'email').send(response);
+  const exist = await User.findOne({ where: {email: req.body.email} });
+  if(exist) return new ErrorMessage('Un compte existe déjà avec cette adresse email !', 'email').send(res);
 
   const salt = await bcrypt.genSalt(12);
-  const hashPassword = await bcrypt.hash(request.body.password, salt);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
 
   const user = new User({
-    email: request.body.email,
+    email: req.body.email,
     password: hashPassword,
-    age: request.body.age,
-    level: request.body.level,
-    nickname: request.body.nickname,
+    age: req.body.age,
+    level: req.body.level,
+    nickname: req.body.nickname,
   });
 
   await user.save();
 
   const token = JWT(user);
 
-  return response.json({
-    token: token,
-  })
+  return res.json({token: token})
 }
 
 module.exports = {

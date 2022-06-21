c931505835644ff92196faaed97c46bf4370f815
BuildTools <sarquentin@gmail.com>
2022-06-21 04:21:39 +0200

    :rocket: Update limit size

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 var cors = require('cors');
 const sequelize = require('./database/connection');
+var bodyParser = require('body-parser')
 
 const Seeders = require('./database/seeds/Seeders')
 
@@ -10,10 +11,9 @@ const app = express();
 sequelize.sync();
 dotenv.config();
 
-app.use(express.json());
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({limit: '100mb'}));
 app.use('/auth', require('./routes/auth'));
 app.use('/area', require('./routes/area'));
 

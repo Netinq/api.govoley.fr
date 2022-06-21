8ddc872022dadf8d41ca7d3aa8293d8415e149eb
BuildTools <sarquentin@gmail.com>
2022-06-21 04:04:17 +0200

    :rocket: Update limit size

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use('/auth', require('./routes/auth'));
 app.use('/area', require('./routes/area'));
 

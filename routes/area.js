46d4051146d85f2a9449ffeb4f756d875be98bf8
BuildTools <sarquentin@gmail.com>
2022-06-23 04:52:23 +0200

    :sparkles: Adding show area feature

--- routes/area.js
+++ routes/area.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const { Store, Near } = require('../api/controllers/areaController')
+const { Store, Near, Show } = require('../api/controllers/areaController')
 
 const AppToken = require('../api/middlewares/AppToken');
 const UserToken = require('../api/middlewares/UserToken');
@@ -14,6 +14,11 @@ router.post('/',
 router.post('/near',
   async (req, res, next) => AppToken(req, res, next),
   async (req, res) => Near(req, res)
+)
+
+router.get('/:area_uuid',
+  async (req, res, next) => AppToken(req, res, next),
+  async (req, res) => Show(req, res)
 )
 
 module.exports = router;

bbccafa8204c03b0ba233200b6af40c823832582
BuildTools <sarquentin@gmail.com>
2022-07-07 17:53:00 +0200

    :ambulance: Fixing association issue

--- database/models/AreaSurfaces.js
+++ database/models/AreaSurfaces.js
@@ -19,4 +19,7 @@ AreaSurfaces.init({
   updatedAt: 'updated_at',
 })
 
+Areas.belongsTo(AreaSurfaces, { foreignKey: 'area_surface', as: 'surface' })
+AreaSurfaces.hasOne(Areas, { foreignKey: 'area_surface' })
+
 module.exports = AreaSurfaces;
